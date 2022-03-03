0eac86c8bd7728d348b8cc5bf0f7c2c69892e638
Anusuya Baby <69139140+anusuyababy@users.noreply.github.com>
2022-03-03 20:59:15 +0400

    Add files via upload

--- codegenerator.py
+++ codegenerator.py
@@ -12,9 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import operator
 import re
-
-import sweetviz 
-
+import dtale
 from IPython.display import display
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -25,9 +23,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report, roc_curve, auc
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report, roc_curve, auc
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score 
@@ -137,24 +132,6 @@ def generate_code(query, df):
     elif operator.contains(query, "categorical variables"):
         featcat=[feature for feature in df.columns if df[feature].dtype =='O']
         s = df[featcat].head(5).to_html()
-        return s
-    elif operator.contains(query, "visualize"):
-        f =list(df.columns.values.tolist())
-        d = f[-1]
-        my_report = sweetviz.analyze([df, "Data"],target_feat=d)
-        s = my_report.show_html('Report.html')
-        return s
-    elif operator.contains(query, "visualise"):
-        my_report = sweetviz.analyze([df, "Data"],target_feat=d)
-        s = my_report.show_html('Report.html')
-        return s
-    elif operator.contains(query, "visualisation"):
-        my_report = sweetviz.analyze([df, "Data"],target_feat=d)
-        s = my_report.show_html('Report.html')
-        return s
-    elif operator.contains(query, "visualization"):
-        my_report = sweetviz.analyze([df, "Data"],target_feat=d)
-        s = my_report.show_html('Report.html')
         return s
     elif operator.contains(query, "missing"):
         df.replace('?', np.NaN, inplace=True)
@@ -306,7 +283,128 @@ def generate_code(query, df):
         X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=d, random_state=0)
         d = X_train.shape, X_test.shape
         return d
-    elif operator.contains(query, "build"):
+    elif operator.contains(query, "forest"):
+        le = LabelEncoder()
+        objList = df.select_dtypes(include = "object").columns
+        print (objList)
+        for feat in objList:
+            df[feat] = le.fit_transform(df[feat].astype(str))
+        x = df.iloc[:,:-1]
+        y = df.iloc[: , -1:].values
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+        # random forest
+        clf1 = RandomForestClassifier(n_estimators=20)
+        clf1.fit(x_train, y_train)
+        rfscore = clf1.score(x_train, y_train)
+        y_pred1=clf1.predict(x_test)
+        q = accuracy_score(y_test,y_pred1)
+        q1 = classification_report(y_test, y_pred1)
+        q2 = confusion_matrix(y_test, y_pred1)
+        q3 = precision_score(y_test, y_pred1, average="micro")
+        q4 = recall_score(y_test, y_pred1, average="micro")
+        q5 = f1_score(y_test, y_pred1)
+        report = pd.DataFrame()
+        report['Model'] = ['Random Forest']
+        report['Training Accuracy'] =[rfscore] 
+        report['Test Accuracy'] = [q]
+        report['Confusion matrix'] = [q2]
+        report['Precision'] = [q3] 
+        report['Recall'] = [q4] 
+        report['F1-score'] = [q5] 
+        z = report.to_html()
+        return z
+    elif operator.contains(query, "tree"):
+        le = LabelEncoder()
+        objList = df.select_dtypes(include = "object").columns
+        print (objList)
+        for feat in objList:
+            df[feat] = le.fit_transform(df[feat].astype(str))
+        x = df.iloc[:,:-1]
+        y = df.iloc[: , -1:].values
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+        # Decision tree classifier
+        clf3 = DecisionTreeClassifier()
+        clf3.fit(x_train, y_train)
+        dtscore = clf3.score(x_train, y_train)
+        y_pred3=clf3.predict(x_test)
+        s = accuracy_score(y_test,y_pred3)
+        s1 = classification_report(y_test, y_pred3)
+        s2 = confusion_matrix(y_test, y_pred3)
+        s3 = precision_score(y_test, y_pred3, average="micro")
+        s4 = recall_score(y_test, y_pred3, average='micro')
+        s5 = f1_score(y_test, y_pred3)
+        report = pd.DataFrame()
+        report['Model'] = ['Decission Tree'] 
+        report['Training Accuracy'] = [dtscore] 
+        report['Test Accuracy'] = [s]
+        report['Confusion matrix'] = [s2]
+        report['Precision'] = [s3]
+        report['Recall'] = [s4]
+        report['F1-score'] = [s5]
+        z = report.to_html()
+        return z
+    elif operator.contains(query, "adaboost"):
+        le = LabelEncoder()
+        objList = df.select_dtypes(include = "object").columns
+        print (objList)
+        for feat in objList:
+            df[feat] = le.fit_transform(df[feat].astype(str))
+        x = df.iloc[:,:-1]
+        y = df.iloc[: , -1:].values
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+        # adaboost classifier
+        clf5 = AdaBoostClassifier()
+        clf5.fit(x_train, y_train)
+        adascore = clf5.score(x_train, y_train)
+        y_pred5=clf5.predict(x_test)
+        u = accuracy_score(y_test,y_pred5)
+        u1 = classification_report(y_test, y_pred5)
+        u2 = confusion_matrix(y_test, y_pred5)
+        u3 = precision_score(y_test, y_pred5, average="micro")
+        u4 = recall_score(y_test, y_pred5, average='micro')
+        u5 = f1_score(y_test, y_pred5)
+        report = pd.DataFrame()
+        report['Model'] = ['Adaboost']
+        report['Training Accuracy'] =[adascore]
+        report['Test Accuracy'] = [u]
+        report['Confusion matrix'] = [u2]
+        report['Precision'] = [u3]
+        report['Recall'] = [u4]
+        report['F1-score'] = [u5]
+        z = report.to_html()
+        return z
+    elif operator.contains(query, "boosting"):
+        le = LabelEncoder()
+        objList = df.select_dtypes(include = "object").columns
+        print (objList)
+        for feat in objList:
+            df[feat] = le.fit_transform(df[feat].astype(str))
+        x = df.iloc[:,:-1]
+        y = df.iloc[: , -1:].values
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+        # gradient boosting
+        clf6 = GradientBoostingClassifier()
+        clf6.fit(x_train, y_train)
+        gradientscore = clf6.score(x_train, y_train)
+        y_pred6=clf6.predict(x_test)
+        v = accuracy_score(y_test,y_pred6)
+        v1 = classification_report(y_test, y_pred6)
+        v2 = confusion_matrix(y_test, y_pred6)
+        v3 = precision_score(y_test, y_pred6, average="micro")
+        v4 = recall_score(y_test, y_pred6, average='micro')
+        v5 = f1_score(y_test, y_pred6)
+        report = pd.DataFrame()
+        report['Model'] = ['Gradient boosting']
+        report['Training Accuracy'] =[gradientscore]
+        report['Test Accuracy'] = [v]
+        report['Confusion matrix'] = [v2]
+        report['Precision'] = [v3]
+        report['Recall'] = [v4]
+        report['F1-score'] = [v5]
+        z = report.to_html()
+        return z
+
+    elif operator.contains(query, "compare"):
         le = LabelEncoder()
         objList = df.select_dtypes(include = "object").columns
         print (objList)
@@ -500,7 +598,7 @@ def generate_code(query, df):
         report['F1-score'] = [p5, q5, r5, s5, t5, u5, v5]
         z = report.to_html()
         return z
-        
+        return z
     elif operator.contains(query, "model training"):
         le = LabelEncoder()
         objList = df.select_dtypes(include = "object").columns
@@ -598,7 +696,7 @@ def generate_code(query, df):
         report['F1-score'] = [p5, q5, r5, s5, t5, u5, v5]
         z = report.to_html()
         return z
-    elif operator.contains(query, "train"):
+    elif operator.contains(query, "train the model"):
         le = LabelEncoder()
         objList = df.select_dtypes(include = "object").columns
         print (objList)
@@ -620,7 +718,7 @@ def generate_code(query, df):
         p5 = f1_score(y_test, y_pred)
         # random forest
         clf1 = RandomForestClassifier(n_estimators=20)
-        clf1.fit(x_train, y_train.values.ravel())
+        clf1.fit(x_train, y_train)
         rfscore = clf1.score(x_train, y_train)
         y_pred1=clf1.predict(x_test)
         q = accuracy_score(y_test,y_pred1)
